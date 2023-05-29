c613d48990a4eeb1b67424709c83909d8ba26e6b
Tesla-g <121763140+Tesla-g@users.noreply.github.com>
2023-05-30 04:05:36 +0530

    2 files modified

--- src/Error.js
+++ src/Error.js
@@ -1,9 +1,10 @@
 //this will show the error when the routing page is not found
-
+import React from 'react';
 const Error = () => {
-  return <div>
-
-
-  
-  </div>; 
+  return (
+    <div className="Errorpage">
+      <h1>erromessage </h1>
+      <h2>errostatuscode</h2>
+    </div>
+  );
 };
