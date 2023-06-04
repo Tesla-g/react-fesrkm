9753d5b46b0b5a14ee130e7b23f2d3170f690d12
Tesla-g <121763140+Tesla-g@users.noreply.github.com>
2023-06-04 23:24:57 +0530

    1 file modified

--- src/Restramenu.js
+++ src/Restramenu.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
+import {IMG_CDN_URL} from "./Content"
 const Restramenu = () => {
   let { id } = useParams();
   const [restlist, setreslist] = useState({});
@@ -11,17 +12,17 @@ const Restramenu = () => {
       'https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=28.6558126&lng=77.2419522&restaurantId=340434'
     );
     const resdata = await data.json();
-    setreslist(resdata);
+    setreslist(resdata.data?.cards);
   }
-  console.log(restlist);
+  console.log(restlist[0]?.card?.card?.info);
+  const imagurl=`${ IMG_CDN_URL}`;
   return (
     <>
       <h1>restrapage id is: {id}</h1>
-
-      {
-
-        
-      }
+      <h2>{restlist[0]?.card?.card?.info?.name}</h2>
+    <h3>{restlist[0]?.card?.card?.info?.city}</h3>
+      <img src={} alt="restrimageid" />
+      <h3></h3>
     </>
   );
 };
